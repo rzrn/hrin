bc89660add961574f1a41ef9ea60ee3b285b8d7c
rzrn <rzrngh@outlook.com>
2026-04-03 00:29:02 +0700

    bin/mkdebug.sh: upload

---
+++ bin/mkdebug.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+DEPFILE=bin/makedepend.mk
+
+if [ ! -e ${DEPFILE} ]
+then
+  touch ${DEPFILE}
+  find src/ -name '*.c' -exec makedepend -f ${DEPFILE} -Iinclude/ -- {} +
+fi
+
+make -f Makefile -f ${DEPFILE} CC=cc CFLAGS="-std=c99 -O0 -Wall -Wextra -pedantic" "$@"
