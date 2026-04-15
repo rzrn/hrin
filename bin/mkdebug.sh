3eb1328e5d32231471377097ff3c009ab1552dfa
rzrn <rzrngh@outlook.com>
2026-04-15 21:09:12 +0700

    bin/mkdebug.sh: add “-g” flag for debug symbols

--- bin/mkdebug.sh
+++ bin/mkdebug.sh
@@ -8,4 +8,4 @@ then
   find src/ -name '*.c' -exec makedepend -f ${DEPFILE} -Iinclude/ -- {} +
 fi
 
-make -f Makefile -f ${DEPFILE} CC=cc CFLAGS="-std=c99 -O0 -Wall -Wextra -pedantic" "$@"
+make -f Makefile -f ${DEPFILE} CC=cc CFLAGS="-g -std=c99 -O0 -Wall -Wextra -pedantic" "$@"
